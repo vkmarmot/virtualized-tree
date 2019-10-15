44aa55b26a45a341b0b6b44fce1f3314070ea7ab
vkmarmot <korneevva@pm.me>
2019-10-15 17:42:43 +0300

    fix tests

--- src/functions.spec.ts
+++ src/functions.spec.ts
@@ -1,32 +1,40 @@
-import {flatTree} from "./functions";
+import { flatTree } from "./functions";
 
 describe("functions", () => {
   test("flatTree", () => {
-    expect(flatTree([
+    expect(
+      flatTree([
+        {
+          children: [
+            {
+              collapsed: false,
+              id: "foo"
+            }
+          ],
+          collapsed: false,
+          id: "bar"
+        },
+        {
+          collapsed: false,
+          id: "baz"
+        }
+      ])
+    ).toEqual([
       {
-        children: [
-          {
-            id: "foo"
-          }
-        ],
-        id: "bar"
-      },
-      {
-        id: "baz"
-      }
-    ])).toEqual([
-      {
+        collapsed: false,
         id: "bar",
         offset: 0
       },
       {
+        collapsed: false,
         id: "foo",
         offset: 1
       },
       {
+        collapsed: false,
         id: "baz",
         offset: 0
       }
-    ])
-  })
-})
+    ]);
+  });
+});
